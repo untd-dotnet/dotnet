991450504f55a3d20211d259fb47b219dced22d4
sagari <nsreevani@corp.untd.com>
2023-09-25 14:36:46 +0530

    Added assign fies

---
+++ myproj/Assignments/Program1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dummy
+{
+    class Program1
+    {
+        static void main()
+        {
+            Console.WriteLine("Enter string");
+            String string1 = Console.ReadLine().ToLower().Trim();
+            Dictionary<Char, int> letter_count_dict = new Dictionary<Char, int>();
+            int max_freq = 0;
+            List<char> max_freq_char = new List<char>();
+            for (int i = 0; i < string1.Length; i++)
+            {
+                if (letter_count_dict.ContainsKey(string1[i]) == true)
+                {
+                    letter_count_dict[string1[i]] += 1;
+
+                    if (letter_count_dict[string1[i]] > max_freq)
+                    {
+                        max_freq = letter_count_dict[string1[i]];
+                    }
+                }
+                else
+                {
+                    letter_count_dict[string1[i]] = 1;
+                }
+
+            }
+
+            foreach (var elem in letter_count_dict)
+            {
+
+                if (elem.Value == max_freq)
+                {
+                    max_freq_char.Add(elem.Key);
+                }
+
+            }
+
+
+            foreach (char item in max_freq_char)
+            {
+                Console.Write("Positions of " + item + " : ");
+                for (int j = 0; j < string1.Length; j++)
+                {
+                    if (string1[j] == item)
+                    {
+                        Console.Write(j + 1 + " ");
+                    }
+                }
+                Console.WriteLine();
+            }
+
+
+        }
+    }
+}
---
+++ myproj/Assignments/Program2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dummy
+{
+    class Program2
+    {
+        static void Main(string[] args)
+        {
+
+            /* String string1 = "Learning C# is a great fun";
+             String substring = "great";*/
+            Console.WriteLine("Enter string");
+            String string1 = Console.ReadLine().Trim();
+            Console.WriteLine("Enter substring");
+            String substring = Console.ReadLine().Trim();
+            int substring_length = substring.Length;
+            StringBuilder part1 = new StringBuilder();
+            StringBuilder part2 = new StringBuilder();
+            int ind = string1.IndexOf(substring);
+            if (ind >= 0)
+            {
+
+                for (int i = 0; i < ind; i++)
+                {
+                    part1.Append(string1[i]);
+                }
+                String spart1 = part1.ToString().Trim();
+                for (int i = ind + substring_length; i < string1.Length; i++)
+                {
+                    part2.Append(string1[i]);
+                }
+                String spart2 = part2.ToString().Trim();
+                Console.WriteLine(spart1 + " " + spart2);
+            }
+            else {
+                Console.WriteLine("Oops!! try with other substring ..");
+            }
+        }
+    }
+}
